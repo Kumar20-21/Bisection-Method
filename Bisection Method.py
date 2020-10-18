c8644eb2c7b5532559b54c978400ac4a18fd17b7
Kumar20-21 <63784975+Kumar20-21@users.noreply.github.com>
2020-10-18 15:40:48 +0530

    Update Bisection Method.py

--- Bisection Method.py
+++ Bisection Method.py
@@ -9,7 +9,6 @@ no_of_iterations = 0
 def recursion(a, b, no_of_iterations):
     x_n = (a+b)/2
     if abs(function(x_n)) <= error:
-        print("The error is ", error)
         print(f'The approximate root is {x_n} and the number of iterations is {no_of_iterations}')
     elif function(x_n) * function(a) < 0:
         no_of_iterations += 1
