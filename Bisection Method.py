0c98918d3021cff4298d8c4643ec35b8524d6c41
Kumar20-21 <63784975+Kumar20-21@users.noreply.github.com>
2020-10-18 13:25:29 +0530

    Add files via upload

---
+++ Bisection Method.py
@@ -0,0 +1,23 @@
+def function(t):
+    return t**3-25.0
+
+
+error = 10**(-6)
+no_of_iterations = 0
+
+
+def recursion(a, b, no_of_iterations):
+    x_n = (a+b)/2
+    no_of_iterations += 1
+    while True:
+        if abs(function(x_n)) <= error:
+            print("The error is ", error)
+            print(f'The approximate root is {x_n} and the number of iterations is {no_of_iterations}')
+            break
+        elif function(x_n)*function(a) < 0:
+            recursion(a, x_n, no_of_iterations)
+        elif function(x_n)*function(b) < 0:
+            recursion(x_n, b, no_of_iterations)
+
+
+recursion(2, 3, no_of_iterations)
